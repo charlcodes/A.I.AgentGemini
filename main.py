2b84f6c33a1eccc0aed3e07decc8251d8a8de18b
charlcodes <charl.codes@gmail.com>
2026-06-17 15:44:06 +0200

    First commit 2026/06/17

---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+import argparse as ap 
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key == None:
+    raise RuntimeError("API key was not fetched, check variables.")
+
+client = genai.Client(api_key=api_key)
+
+
+
+def main():
+    parser = ap.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    # trying to keep chat list resubmit
+    messages: list[types.Content] = [
+    types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+]
+
+    #user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    #print("Hello from ai-agent!")
+    response = client.models.generate_content(
+    model='gemini-2.5-flash',
+    contents= messages
+)
+    if response.usage_metadata:
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+    else:
+        raise RuntimeError("Meta data is None")
+    
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
+        message = f"Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}"
+        print(message)
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
