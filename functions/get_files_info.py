7e47a0fc0d4d7ca74b36f6c269c256c4b76753c0
charlcodes <charl.codes@gmail.com>
2026-06-17 18:09:02 +0200

    Added doc file itterator

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -6,8 +6,10 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         os.path.join(): Join two paths together safely (handles slashes)
         os.path.normpath(): Normalize a path (handles things like ..)
         os.path.commonpath(): Get the common sub-path shared by multiple paths
-        os.path.isdir(): Check if a path points to an existing directory
+        os.path.isdir(): Check if a path points to an existing directory 
     """
+
+    """path validation"""
     try:
         abs_pth_wrk_dir: str = os.path.abspath(working_directory)
         target_dir: str = os.path.normpath(os.path.join(abs_pth_wrk_dir, directory))
@@ -17,9 +19,23 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         
     if not valid_target_dir:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not Path(directory).is_dir():
+    if not Path(target_dir).is_dir():
         return f'Error: "{directory}" is not a directory'
+    
+    #if success
     else:
-        return f'Success: "{directory}" is within the working directory'
+        #return f'Success: "{directory}" is within the working directory'
+        #dir_path = Path(directory)
+        #for item in dir_path.iterdir():
+        items_in_dir = []
+        try:
+            for entry in os.scandir(target_dir):
+                items_in_dir.append(f"  - {entry.name}: file_size={entry.stat().st_size} bytes, is_dir={entry.is_dir()}")
+            return f"Result for {'current' if directory == '.' else directory} directory:\n" + '\n'.join(items_in_dir)
+
+        except Exception as e:
+            return f'Error: {e}'
+    
+
 
 
--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,7 +1,8 @@
 from functions.get_files_info import get_files_info
 
 test0 = get_files_info("calculator", ".")
-test1 = get_files_info("calculator", "/bin")
-test2 = get_files_info("calculator", "../")
-test3 = get_files_info("calculator", "main.py")
-print(f"{test0}\n{test1}\n{test2}\n{test3}")
+test1 = get_files_info("calculator", "pkg")
+test2 = get_files_info("calculator", "/bin")
+test3 = get_files_info("calculator", "../")
+#test4 = get_files_info("calculator", "main.py")
+print(f"{test0}\n{test1}\n{test2}\n{test3}") #\n{test4}
